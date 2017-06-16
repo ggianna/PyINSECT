f6b71657350a23ca2f8a5ee281b0e88fa91fd1d1
ggianna <ggianna@scify.org>
2017-06-16 18:19:37 +0300

    - Added return statement in recursion parts of LtoR and parallel operators. - TODO: Check if missing elsewhere

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import pdb
 from documentModel import *
 
-ngg1 = DocumentNGramGraph(3,2,"abcdef")
-ngg2 = DocumentNGramGraph(3,2,"abcdeff")
+
+#ngg1 = DocumentNGramGraph(3,2,"abcdef")
+#ngg2 = DocumentNGramGraph(3,2,"abcdeff")
+ngg1 = DocumentNGramGraph(3,2,"A test")
+ngg2 = DocumentNGramGraph(3,2,"Another test test")
 #ngg1.GraphDraw()
 #ngg2.GraphDraw()
 gs = SimilarityNVS()
@@ -15,10 +18,8 @@ nop = LtoRNary(gs)
 print gs.apply(ngg1,ngg2)
 bop = Union(lf=0.5, commutative=True,distributional=True)
 nop = LtoRNary(bop)
-# strange error still here
-# maybe dependent with 
-# garbage collecting
-# functions arguments
-# as fields on ar's
-bop.apply(ngg1,ngg2).GraphDraw()
+pop = ParallelNary(bop)
+
+nop.apply(ngg1,ngg2).GraphDraw()
+
 
