7468f9e38fbbc493303ac19738803d7ae3fa0edc
ysig <y.siglidis@gmail.com>
2017-07-03 23:26:49 +0300

    README + dependencies install in .sh format

---
+++ README.md
@@ -0,0 +1,2 @@
+A represantation of N-grams-graphs in python 
+notionally connencted with the JInsect toolkit
---
+++ install_dependencies.sh
@@ -0,0 +1,24 @@
+#PROJECT IS NOW IN: STUDY VERSION
+
+## NgramGraph Dependencies:
+
+# Install pip
+sudo apt-get install python-pip
+
+
+# Install python graph library "Networkx"
+sudo pip install networkx
+
+
+# Install MathPlot lib
+sudo apt-get install python-dev 
+sudo apt-get install python-tk
+sudo python -m pip install matplotlib 
+
+# Install Pydot, PydotPlus
+sudo pip install pydot
+sudo pip install pydotplus
+
+# Install python graph viz library "pygraphviz"
+sudo apt-get install graphviz libgraphviz-dev pkg-config
+sudo pip install pygraphviz --install-option="--include-path=/usr/include/graphviz" --install-option="--library-path=/usr/lib/graphviz/"
