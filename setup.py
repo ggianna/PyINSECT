ff51af40d3dec83bca243cc2981b3390ea93f454
npit <pittarasnikif@iit.demokritos.gr>
2020-05-06 01:17:43 +0300

    Add comparators package

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyinsect-ggianna",
-    version="0.0.38",
+    version="0.0.39",
     author="George Giannakopoulos",
     author_email="ggianna@iit.demokritos.gr",
     description="A python port of the JInsect toolkit",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/npit/PyINSECT",
-    packages=['pyinsect', 'pyinsect.collector', 'pyinsect.documentModel', 'pyinsect.documentModel.representations'],
+    packages=['pyinsect', 'pyinsect.collector', 'pyinsect.documentModel', 'pyinsect.documentModel.representations', 'pyinsect.documentModel.comparators'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
