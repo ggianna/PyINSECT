2e53b763e114860bffdd867f0b6ac04c8fc61442
billsioros <billsioros97@gmail.com>
2021-05-20 20:10:19 +0300

    refactor: formatted via `black` and `isort`

--- src/pyinsect/documentModel/representations/__init__.py
+++ src/pyinsect/documentModel/representations/__init__.py
@@ -1,4 +1,10 @@
-from pyinsect.documentModel.representations.DocumentNGramGaussNormGraph import DocumentNGramGaussNormGraph
+from pyinsect.documentModel.representations.DocumentNGramGaussNormGraph import (
+    DocumentNGramGaussNormGraph,
+)
 from pyinsect.documentModel.representations.DocumentNGramGraph import DocumentNGramGraph
-from pyinsect.documentModel.representations.DocumentNGramHGraph import DocumentNGramHGraph
-from pyinsect.documentModel.representations.DocumentNGramSymWinGraph import DocumentNGramSymWinGraph
+from pyinsect.documentModel.representations.DocumentNGramHGraph import (
+    DocumentNGramHGraph,
+)
+from pyinsect.documentModel.representations.DocumentNGramSymWinGraph import (
+    DocumentNGramSymWinGraph,
+)
