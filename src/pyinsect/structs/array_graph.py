c904ada78611612ab5ac1a5df85f5bcc5d649d8b
billsioros <billsioros97@gmail.com>
2021-05-25 21:15:57 +0300

    feat: add stride to `ArrayGraph`

--- src/pyinsect/structs/array_graph.py
+++ src/pyinsect/structs/array_graph.py
@@ -4,11 +4,12 @@ import networkx
 
 
 class ArrayGraph(ABC):
-    def __init__(self, data, window_size) -> None:
+    def __init__(self, data, window_size, stride=1) -> None:
         super().__init__()
 
         self._data = data
         self._window_size = window_size
+        self._stride = stride
 
     def __len__(self):
         try:
@@ -17,7 +18,9 @@ class ArrayGraph(ABC):
             return 0
 
     def __str__(self):
-        return "length: {0}, window size: {1}".format(len(self), self._window_size)
+        return "length: {0}, window size: {1}, stride: {2}".format(
+            len(self), self._window_size, self._stride
+        )
 
     def __repr__(self) -> str:
         return '<{0} "{1}">'.format(self.__class__.__name__, str(self))
@@ -29,6 +32,10 @@ class ArrayGraph(ABC):
     @property
     def window_size(self):
         return self._window_size
+
+    @property
+    def stride(self):
+        return self._stride
 
     @abstractmethod
     def as_graph(self, graph_type, *args, **kwargs):
@@ -48,8 +55,8 @@ class ArrayGraph2D(ArrayGraph):
     def _process_patch(self, x, y):
         neighbors = []
 
-        for neighbor_y in range(*self._get_window(y)):
-            for neighbor_x in range(*self._get_window(x)):
+        for neighbor_y in range(*self._get_window(y), self._stride):
+            for neighbor_x in range(*self._get_window(x), self._stride):
                 if neighbor_x != x and neighbor_y != y:
                     neighbors.append((neighbor_y, neighbor_x))
 
