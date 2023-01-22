b6cf9fc6574d088d3fd720d71fc8ff1f8c996afa
George Giannakopoulos <ggianna@scify.org>
2023-01-22 16:01:30 +0200

    - Fixes in all ngram graphs similarity calculation. - Re-engineered test file for NGGs and operators.

--- src/tests/test_ngram_graphs.py
+++ src/tests/test_ngram_graphs.py
@@ -1,10 +1,11 @@
 import sys, os
-from networkx.readwrite import json_graph
-import json
 import networkx as nx
 import matplotlib.pyplot as plt
 import logging as log
+from typing import Any
 
+# Verbose logging
+log.basicConfig(level=log.INFO)
 
 # Add module to search path
 sDir = os.path.dirname(__file__) + "/.."
@@ -12,11 +13,11 @@ print(f"Adding {sDir} to search path...")
 sys.path.insert(0, sDir)
 
 # Init logging
-import pyinsect.utils.logging as pyi_log
-pyi_log.init_logging(level=log.INFO)
+import pyinsect.utils.logging
 
 # Import classes
 from pyinsect.graphs.ngram_graphs import (AsymmetricNGramGraph, SymmetricNGramGraph, ProximityGraph)
+import pyinsect.graphs.operators.proximity_graph_operators as pgops
 
 
 def test_ngram_graph_creation():
@@ -46,7 +47,7 @@ def test_ngram_graph_creation():
 
     my_str = "1234567"
     # Asymmetric tests
-    log.log(log.INFO, "Asymmetric tests...")
+    log.info("Asymmetric graph tests...")
     my_graph = AsymmetricNGramGraph(data=my_str)
     draw_graph_with_weights(my_graph)
 
@@ -61,10 +62,10 @@ def test_ngram_graph_creation():
 
     my_graph = AsymmetricNGramGraph(data=my_str, n=10, Dwin=3)
     draw_graph_with_weights(my_graph)
-    log.log(log.INFO, "Asymmetric tests... Done.")
+    log.info("Asymmetric graph tests... Done.")
 
     # Symmetric tests
-    log.log(log.INFO, "Symmetric tests...")
+    log.info("Symmetric graph tests...")
     my_graph = SymmetricNGramGraph(data=my_str)
     draw_graph_with_weights(my_graph)
 
@@ -79,11 +80,46 @@ def test_ngram_graph_creation():
 
     my_graph = SymmetricNGramGraph(data=my_str, n=10, Dwin=3)
     draw_graph_with_weights(my_graph)
-    log.log(log.INFO, "Symmetric tests... Done.")
+    log.info("Symmetric graph tests... Done.")
 
 
 def test_ngram_graph_comparison():
-    ag1 = AsymmetricNGramGraph()
+    def output_similarity(simop : pgops.BaseSimilarityOperator, to_print: pgops.Similarity, obj1name : str = "Obj1", obj2name : str = "Obj2"):
+        print(f"+++ Reference object name: {obj1name}; Evaluated object name: {obj2name};\nSimilarity type: {simop.__class__}; Value: {to_print.value}; Components: {to_print.components}")
+
+    def output_variations(simop : pgops.BaseSimilarityOperator, ag1, ag2, agIrr, data1, data2, dataIrr):
+        # Different overlapping
+        output_similarity(simop, simop.similarity(ag1, ag2), data1, data2)
+        # Reversed different overlapping
+        output_similarity(simop, simop.similarity(ag2, ag1), data2, data1)
+        # Irrelevant
+        output_similarity(simop, simop.similarity(ag1, ag1), data2, data1)
+        # Self-similarity
+        output_similarity(simop, simop.similarity(ag1, agIrr), data1, dataIrr)
+    
+    # Init strings
+    data1 = "123456"
+    data2 = "3456789345456"
+    dataIrr = "hello world!"
+
+    # For each graph type
+    for cur_class in [AsymmetricNGramGraph, SymmetricNGramGraph]:
+        log.info(f"+++ Tests for graph class {str(cur_class)}...")
+        ag1 = cur_class(data=data1)
+        ag2 = cur_class(data=data2)
+        agIrr = cur_class(data=dataIrr)
+
+        # For each similarity type
+        for simop_class in [pgops.SizeSimilarity, pgops.SymmetricContainmentSimilarity, pgops.AsymmetricContainmentSimilarity, 
+            pgops.SymmetricValueSimilarity, pgops.AsymmetricValueSimilarity]:
+            log.info(f"+++ +++Examining similarity {str(simop_class)}...")
+            simop = simop_class()
+
+            output_variations(simop, ag1, ag2, agIrr, data1, data2, dataIrr)
+
+        log.info(f"--- Tests for class {str(cur_class)}... Done.")
+
+    log.info("Symmetric tests... Done.")
 
 
 def main():
